4cbe499fce18de6bd2837cccbc419edc9dbb9f52
HyunJune Kang <killa5294@gmail.com>
2022-12-29 21:19:13 +0900

    feat : Player 기본 이동 구현
    플레이어 이동을 게임에 적합한 이동으로 구현하기 위해 CharacterController 방식을 사용하였다.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,7 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
+    private CharacterController controller;
 
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float jumpPower;
+    private float moveY;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        Vector3 moveInput = Vector3.forward * Input.GetAxis("Vertical") + Vector3.right * Input.GetAxis("Horizontal");
+
+
+        controller.Move(moveInput * moveSpeed * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        moveY += Physics.gravity.y * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            moveY = jumpPower;
+        }
+        else if (controller.isGrounded && moveY < 0)
+        {
+            moveY = 0;
+        }
+
+        controller.Move(Vector3.up * moveY * Time.deltaTime);
+    }
 }
