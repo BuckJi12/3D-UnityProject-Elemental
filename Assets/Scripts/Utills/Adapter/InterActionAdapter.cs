fb2b0f3a0b3d5e9d2a98df5d27e8ebcc488338d3
HyunJune Kang <killa5294@gmail.com>
2022-12-30 17:58:50 +0900

    feat : 상호작용 어댑터 구현
    IInterfaceable 인터페이스를 어디든 붙여 사용할 수 있도록 어댑터 패턴으로 구현하였다.

---
+++ Assets/Scripts/Utills/Adapter/InterActionAdapter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InterActionAdapter : MonoBehaviour, IInterActionable
+{
+    public UnityEvent<PlayerColliders> OnInterAction;
+
+    public void InterAction(PlayerColliders collider)
+    {
+            OnInterAction?.Invoke(collider);
+    }
+}
+
