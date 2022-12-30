f8a9a17b5f2f87e2546b0043dfff346f624bc301
HyunJune Kang <killa5294@gmail.com>
2022-12-30 17:54:03 +0900

    feat : Player 상호 작용 구현
    플레이어의 일정 범위 안에 들어오면 상호작용이 가능하도록 기능을 구현하였다.

--- Assets/Scripts/Player/PlayerColliders.cs
+++ Assets/Scripts/Player/PlayerColliders.cs
@@ -4,5 +4,36 @@ using UnityEngine;
 
 public class PlayerColliders : MonoBehaviour
 {
+    [SerializeField]
+    private float interActionRange;
+    [SerializeField]
+    private bool interActionGizmos;
 
+    private void Update()
+    {
+        InterAction();
+    }
+
+    private void InterAction()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, interActionRange);
+        
+        if (colliders == null)
+            return;
+
+        IInterActionable target = colliders[0].GetComponent<IInterActionable>();
+        if (Input.GetButtonDown("InterAction"))
+        {
+            target?.InterAction(this);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (interActionGizmos)
+        {
+            Gizmos.color = Color.white;
+            Gizmos.DrawWireSphere(transform.position, interActionRange);
+        }
+    }
 }
