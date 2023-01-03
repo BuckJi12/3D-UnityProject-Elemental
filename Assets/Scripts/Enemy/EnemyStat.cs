45b260393f1dedce1efef3e778f0c9adec7792a3
HyunJune Kang <killa5294@gmail.com>
2023-01-03 20:04:11 +0900

    feat : EnemyStat 구현
    Enemy의 체력과 MonsterData를 자료를 보관해 줄 클래스를 구현 했다.

---
+++ Assets/Scripts/Enemy/EnemyStat.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStat : MonoBehaviour
+{
+    public MonsterData statData;
+    public int curHP;
+    public int maxHP;
+}
