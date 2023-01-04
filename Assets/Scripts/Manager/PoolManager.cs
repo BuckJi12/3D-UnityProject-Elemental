5030968ddff77a9b5ac7885f8d06b30c1658c252
Hyunjune Kang <killa5294@gmail.com>
2023-01-05 02:11:18 +0900

    feat: 풀매니저 구현
    게임 오브젝트의 생성과 삭제 과정에서 가비지 컬렉터의 호출을 줄여주기 위한 풀매니저를 구현했다

---
+++ Assets/Scripts/Manager/PoolManager.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : SingleTon<PoolManager>
+{
+    private Dictionary<string, Stack<GameObject>> poolDic;
+
+    [SerializeField]
+    private List<Poolable> poolPrefab;
+
+    private void Awake()
+    {
+        poolDic = new Dictionary<string, Stack<GameObject>>();
+    }
+
+    private void Start()
+    {
+        CreatePool();
+    }
+    public void CreatePool()
+    {
+        for (int i = 0; i < poolPrefab.Count; i++)
+        {
+            Stack<GameObject> stack = new Stack<GameObject>();
+            for (int j = 0; j < poolPrefab[i].count; j++)
+            {
+                GameObject instance = Instantiate(poolPrefab[i].prefab);
+                instance.SetActive(false);
+                instance.gameObject.name = poolPrefab[i].prefab.name;
+                instance.transform.parent = poolPrefab[i].container;
+                stack.Push(instance);
+            }
+            poolDic.Add(poolPrefab[i].prefab.name, stack);
+        }
+    }
+
+    public GameObject Get(GameObject prefab)
+    {
+        Stack<GameObject> stack = poolDic[prefab.name];
+        if (stack.Count > 0)
+        {
+            GameObject instance = stack.Pop();
+            instance.SetActive(true);
+            instance.transform.parent = null;
+            return instance;
+        }
+
+        Poolable poolable = poolPrefab.Find((x) => prefab.name == x.prefab.name);
+        if (poolable.enableCreation)
+        {
+            GameObject instance = Instantiate(prefab);
+            instance.SetActive(true);
+            instance.gameObject.name = prefab.name;
+            instance.transform.parent = null;
+            return instance;
+        }
+
+        else { return null; }
+    }
+
+    public void Release(GameObject instance)
+    {
+        Stack<GameObject> stack = poolDic[instance.name];
+        instance.SetActive(false);
+        stack.Push(instance);
+
+        Poolable poolable = poolPrefab.Find((x) => instance.name == x.container.name);
+        instance.transform.parent = poolable.container;
+    }
+
+    [Serializable]
+    public struct Poolable
+    {
+        public GameObject prefab;
+        public int count;
+        public Transform container;
+        public bool enableCreation;
+    }
+}
