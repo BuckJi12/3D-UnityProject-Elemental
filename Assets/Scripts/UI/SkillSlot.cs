d60324afc948b4bd45f86438dde050e8a6a814fc
HyunJune Kang <killa5294@gmail.com>
2023-01-10 17:24:21 +0900

    feat: SkillSlot 구현
    가지고 있는 스킬들이 스킬창에 표시될 때 스킬들의 아이콘들을 표시해줄 클래스 구현

---
+++ Assets/Scripts/UI/SkillSlot.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillSlot : MonoBehaviour
+{
+    [SerializeField]
+    private Image skillIcon;
+
+    private Skill skill;
+
+    private void Start()
+    {
+        skillIcon = GetComponent<Image>();
+        skillIcon.enabled = false;
+    }
+
+    public void OnSkill(Skill skill)
+    {
+        skillIcon.gameObject.SetActive(true);
+        this.skill = skill;
+        skillIcon.sprite = skill.data.skillIcon;
+        skillIcon.enabled = true;
+    }
+}
+
