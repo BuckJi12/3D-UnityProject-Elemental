ebe0b6f850959a5eccb5a8613d12df024adca2d7
HyunJune Kang <killa5294@gmail.com>
2023-01-11 20:37:37 +0900

    refactor: Particle 코드 변경
    플레이어 자식에 있던 파티클이 플레이어가 회전하면 같이 회전하는 현상이 일어나 풀매니저가 관리해주고 사용할 때 빌려주는 형식으로 변경했다

--- Assets/Scripts/Player/PlayerParticle.cs
+++ Assets/Scripts/Player/PlayerParticle.cs
@@ -1,17 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerParticle : MonoBehaviour
 {
     [SerializeField]
-    private ParticleSystem iceBreak;
+    private GameObject iceBreak;
     [SerializeField]
-    private ParticleSystem fireDischarge;
+    private GameObject fireDischarge;
 
 
-    public void OnParticle(ParticleSystem particle)
+    public void OnParticleIceBreak()
     {
-        particle.Play();
+        ParticleSystem instance = PoolManager.Instance.Get(iceBreak).GetComponent<ParticleSystem>();
+        instance.transform.position = transform.position;
+        instance.transform.rotation = transform.rotation;
+        instance.transform.Rotate(Vector3.up * -90);
+        instance.Play();
+    }
+
+    public void OnParticleFireDischarge()
+    {
+        ParticleSystem instance = PoolManager.Instance.Get(fireDischarge).GetComponent<ParticleSystem>();
+        instance.transform.position = transform.position;
+        instance.Play();
     }
 }
