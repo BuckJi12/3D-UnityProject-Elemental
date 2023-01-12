145438a627d4b8b411119e14e60f6deafa96697e
HyunJune Kang <killa5294@gmail.com>
2023-01-12 18:00:38 +0900

    feat: State 클래스 구현
    유한상태머신에 필수로 정의해야되는 진입, 업데이트, 종료 시점의 함수를 구현하기 위한 상태 클래스를 구현하였다

---
+++ Assets/Scripts/Utills/DesignPattern/StateMachine/State.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Compilation;
+using UnityEngine;
+
+public abstract class State<T> where T : class
+{
+    public abstract void Enter(T entity);
+
+    public abstract void Update(T entity);
+
+    public abstract void Exit(T entity);
+}
