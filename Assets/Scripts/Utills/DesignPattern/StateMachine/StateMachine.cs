cc4e2e1fef94ff0be0f872465fce6afc63d81f07
HyunJune Kang <killa5294@gmail.com>
2023-01-12 18:20:14 +0900

    feat: Finite State Machine 제너릭 클래스 구현
    유한상태머신의 다양성을 확보한 클래스를 구현하였다

---
+++ Assets/Scripts/Utills/DesignPattern/StateMachine/StateMachine.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine<T> where T : class
+{
+    private T owner;
+    private State<T> curState;
+    private State<T> beforeState;
+
+    public void Init(T owner, State<T> startState)
+    {
+        this.owner = owner;
+        curState = null;
+        beforeState = null;
+    }
+
+    public void Update()
+    {
+        if (curState == null)
+            return;
+
+        curState.Update(owner);
+    }
+
+    public void ChangeState(State<T> newState)
+    {
+        if (newState == null)
+            return;
+
+        if (curState != null)
+        {
+            curState.Exit(owner);
+            beforeState = curState;   
+        }
+
+        curState = newState;
+        curState.Enter(owner);
+    }
+}
