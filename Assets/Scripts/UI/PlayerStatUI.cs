71926ecbc7134b5e70a700ecadf6addf2f46be1a
Hyunjune Kang <killa5294@gmail.com>
2023-01-17 02:02:58 +0900

    feat: Player Stat UI 구현
    플레이어의 체력과 스태미너 경험치를 실시간으로 표시해줄 UI를 구현하였다

---
+++ Assets/Scripts/UI/PlayerStatUI.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatUI : MonoBehaviour
+{
+    [SerializeField]
+    private Image hp;
+    [SerializeField]
+    private TextMeshProUGUI hpValue;
+    [SerializeField]
+    private Image stamina;
+    [SerializeField]
+    private TextMeshProUGUI staminaValue;
+    [SerializeField]
+    private Image exp;
+    [SerializeField]
+    private TextMeshProUGUI expValue;
+
+    private void Update()
+    {
+        hp.fillAmount = (float)PlayerStatManager.Instance.stat.curHP / (float)PlayerStatManager.Instance.stat.maxHP;
+        stamina.fillAmount = (float)PlayerStatManager.Instance.stat.curStamina / (float)PlayerStatManager.Instance.stat.maxStamina;
+        exp.fillAmount = (float)PlayerStatManager.Instance.stat.curEXP / (float)PlayerStatManager.Instance.stat.maxEXP;
+
+        hpValue.text = PlayerStatManager.Instance.stat.curHP.ToString() + " / " + PlayerStatManager.Instance.stat.maxHP.ToString();
+        staminaValue.text = PlayerStatManager.Instance.stat.curStamina.ToString() + " / " + PlayerStatManager.Instance.stat.maxStamina.ToString();
+        expValue.text = PlayerStatManager.Instance.stat.curEXP.ToString() + " / " + PlayerStatManager.Instance.stat.maxEXP.ToString();
+    }
+}
