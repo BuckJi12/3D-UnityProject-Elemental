efe58954e05aa3ff701fc8aa088742ab69fcdc3a
Hyunjune Kang <killa5294@gmail.com>
2023-01-23 00:29:18 +0900

    feat: Skill Info UI 구현
    스킬창에서 마우스를 스킬에 올렸을 때 스킬의 정보를 알 수 있는 UI를 구현

--- Assets/Scripts/UI/SkillSlot.cs
+++ Assets/Scripts/UI/SkillSlot.cs
@@ -1,15 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class SkillSlot : MonoBehaviour
+public class SkillSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField]
     private Image skillIcon;
+    [SerializeField]
+    private Image infoUI;
 
+    [SerializeField]
+    private Image infoIcon;
+    [SerializeField]
+    private TextMeshProUGUI title;
+    [SerializeField]
+    private TextMeshProUGUI description;
+    [SerializeField]
+    private TextMeshProUGUI type;
+    [SerializeField]
+    private TextMeshProUGUI damage;
+    [SerializeField]
+    private TextMeshProUGUI level;
+
+    [HideInInspector]
     public Skill skill;
-
+    
     private void Start()
     {
         skillIcon = GetComponent<Image>();
@@ -22,6 +40,27 @@ public class SkillSlot : MonoBehaviour
         this.skill = skill;
         skillIcon.sprite = skill.data.skillIcon;
         skillIcon.enabled = true;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (skill != null)
+        {
+            infoUI.transform.position = Input.mousePosition + new Vector3(300, -300);
+            infoUI.gameObject.SetActive(true);
+            Debug.Log(infoIcon);
+            infoIcon.sprite = skill.data.skillIcon;
+            title.text = skill.data.name;
+            description.text = "설명 : " + skill.data.description;
+            type.text = "원소타입 : " + skill.data.type.ToString();
+            damage.text = "데미지 : " + skill.data.damage.ToString() + "00%";
+            level.text = "레벨 제한 : " + skill.data.canLevel.ToString() + "이상";
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        infoUI.gameObject.SetActive(false);
     }
 }
 
